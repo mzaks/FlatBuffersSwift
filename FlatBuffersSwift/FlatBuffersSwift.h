8ed9c0cb17cda847555587b555b9f7a70a4be709
Maxim Zaks <maxim.zaks@gmail.com>
2015-12-29 13:37:55 +0100

    Initial Commit

---
+++ FlatBuffersSwift/FlatBuffersSwift.h
@@ -0,0 +1,19 @@
+//
+//  FlatBuffersSwift.h
+//  FlatBuffersSwift
+//
+//  Created by Maxim Zaks on 29.12.15.
+//  Copyright © 2015 maxim.zaks. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for FlatBuffersSwift.
+FOUNDATION_EXPORT double FlatBuffersSwiftVersionNumber;
+
+//! Project version string for FlatBuffersSwift.
+FOUNDATION_EXPORT const unsigned char FlatBuffersSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlatBuffersSwift/PublicHeader.h>
+
+
