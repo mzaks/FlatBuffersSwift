604dcd254f0beb850a93dcc92fa45333c0260e8e
Joakim Hassila <joj@mac.com>
2016-05-03 07:49:12 +0200

    Avoid creating bridged strings during decoding.

--- FlatBuffersSwift/FlatBufferReader.swift
+++ FlatBuffersSwift/FlatBufferReader.swift
@@ -72,6 +72,7 @@ public final class FlatBufferReader {
     }
     
     var stringCache : [Int32:String] = [:]
+    var stringBuffer : [UInt8] = []
     
     public func getString(stringOffset : Offset?) -> String? {
         guard let stringOffset = stringOffset else {
@@ -85,8 +86,17 @@ public final class FlatBufferReader {
         
         let stringPosition = Int(stringOffset)
         let stringLength : Int32 = fromByteArray(stringPosition)
-        let pointer = UnsafeMutablePointer<UInt8>(buffer).advancedBy((stringPosition + strideof(Int32)))
-        let result = String.init(bytesNoCopy: pointer, length: Int(stringLength), encoding: NSUTF8StringEncoding, freeWhenDone: false)
+
+        // This slightly convoluted way makes sure we construct a native Swift string instead of a bridged NSString
+
+        stringBuffer.reserveCapacity(Int(stringLength))
+        for i in 0..<stringLength {
+            let pointer = UnsafeMutablePointer<UInt8>(buffer).advancedBy((stringPosition + strideof(Int32) + Int(i)))
+            stringBuffer.append(pointer.memory)
+        }
+        let result = String(utf8: stringBuffer)
+        stringBuffer.removeAll(keepCapacity: true)
+
         if config.uniqueStrings {
             stringCache[stringOffset] = result
         }
--- FlatBuffersSwift/FlatBuffers.swift
+++ FlatBuffersSwift/FlatBuffers.swift
@@ -41,3 +41,30 @@ extension Int : Scalar {}
 extension UInt : Scalar {}
 extension Float32 : Scalar {}
 extension Float64 : Scalar {}
+
+// String extension from Mike Ash for conveniently creating native Swift strings from UTF8 sequences
+// https://www.mikeash.com/pyblog/friday-qa-2015-11-06-why-is-swifts-string-api-so-hard.html
+
+extension String {
+    init?<Seq: SequenceType where Seq.Generator.Element == UInt16>(utf16: Seq) {
+        self.init()
+        
+        guard transcode(UTF16.self,
+                        UTF32.self,
+                        utf16.generate(),
+                        { self.append(UnicodeScalar($0)) },
+                        stopOnError: true)
+            == false else { return nil }
+    }
+    
+    init?<Seq: SequenceType where Seq.Generator.Element == UInt8>(utf8: Seq) {
+        self.init()
+        
+        guard transcode(UTF8.self,
+                        UTF32.self,
+                        utf8.generate(),
+                        { self.append(UnicodeScalar($0)) },
+                        stopOnError: true)
+            == false else { return nil }
+    }
+}
