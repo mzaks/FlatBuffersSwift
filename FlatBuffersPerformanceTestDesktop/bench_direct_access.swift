9d7d87dc28e89ae21ad66dfc50694dacffd396ac
Joakim Hassila <joj@mac.com>
2016-05-11 09:48:22 +0200

    Trying a struct interface for nicer 'streaming' API.

--- FlatBuffersPerformanceTestDesktop/bench_direct_access.swift
+++ FlatBuffersPerformanceTestDesktop/bench_direct_access.swift
@@ -133,3 +133,55 @@ func getPostfixFrom(buffer : UnsafePointer<UInt8>, fooBarOffset : Offset) -> UIn
     let result : UInt8 = get(buffer, fooBarOffset, propertyIndex: 3)!
     return result
 }
+
+// Showcase of using a struct interface
+
+struct FooBarStruct {
+    var buffer : UnsafePointer<UInt8> = nil
+    var myOffset : Offset = 0
+    var name: UnsafeBufferPointer<UInt8> { get { return getStringBuffer(buffer, getOffset(buffer, myOffset, propertyIndex: 1))! } }
+    var rating: Float64 { get { return get(buffer, myOffset, propertyIndex: 2)! } }
+    var postfix: UInt8 {  get { return get(buffer, myOffset, propertyIndex: 3)! } }
+    var sibling: Bar {  get { return get(buffer, myOffset, propertyIndex: 0)! } }
+}
+
+struct FooBarContainerStruct {
+    var buffer : UnsafePointer<UInt8> = nil
+    var myOffset : Offset = 0
+
+    init(_ data : UnsafePointer<UInt8>) {
+        self.buffer = data
+        self.myOffset = UnsafePointer<Offset>(buffer.advancedBy(0)).memory
+        self.list = ListStruct(buffer: buffer, myOffset: myOffset) // set up vector
+    }
+    
+    // table properties
+    var list : ListStruct
+    var location: UnsafeBufferPointer<UInt8> { get { return getStringBuffer(buffer, getOffset(buffer, myOffset, propertyIndex: 3))! } }
+    var fruit: Enum {  get { return Enum(rawValue: get(buffer, myOffset, propertyIndex: 2, defaultValue: Enum.Apples.rawValue))! } }
+    var initialized: Bool {  get { return get(buffer, myOffset, propertyIndex: 1, defaultValue: false) } }
+
+    // definition of table vector to provice nice subscripting etc
+    struct ListStruct {
+        var buffer : UnsafePointer<UInt8> = nil
+        var myOffset : Offset = 0
+        let offsetList : Offset?
+        init(buffer b: UnsafePointer<UInt8>, myOffset o: Offset )
+        {
+            buffer = b
+            myOffset = o
+            offsetList = getOffset(buffer, myOffset, propertyIndex: 0) // cache to make subscript faster
+        }
+        var count : Int { get { return getVectorLength(buffer, offsetList) } }
+        subscript (index : Int) -> FooBarStruct {
+            let ofs = getVectorOffsetElement(buffer, offsetList!, index: index)!
+            return FooBarStruct(buffer: buffer, myOffset: ofs)
+        }
+    }
+    
+    // Convenience to get a shared object for long-term usage
+    func createInstance (reader : FlatBufferReader) -> FooBarContainer {
+        return FooBarContainer.fromFlatBufferReader(reader)
+    }
+}
+
