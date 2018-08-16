9cc666969177b07df7afdc4c1711b3e4e08d6298
Maxim Zaks <maxim.zaks@gmail.com>
2018-08-16 14:59:38 +0200

    Better console output

--- CodeGen/main.swift
+++ CodeGen/main.swift
@@ -8,12 +8,8 @@
 
 import Foundation
 
-print(CommandLine.argc)
-
-print(CommandLine.arguments)
-
 if CommandLine.arguments.count < 2 {
-    print("Pleas provide path to .fbs file as first!!!")
+    print("⚠️ Please provide path to .fbs file as first argument")
     exit(1)
 }
 
@@ -22,19 +18,21 @@ let fbsPath = CommandLine.arguments[1]
 let fbsUrl = URL(fileURLWithPath: fbsPath)
 
 guard let fileContent = try?Data(contentsOf: fbsUrl) else {
-    print("Could not read content of the .fbs file \(fbsUrl)")
+    print("⚠️ Could not read content of the .fbs file \(fbsUrl)")
     exit(1)
 }
 
-print(fbsPath)
+
 
 if CommandLine.arguments.count < 3 {
-    print("Pleas provide path to .swift file as second argument!!!")
+    print("⚠️ Please provide path to .swift file (which you want to generate) as second argument")
     exit(1)
 }
 
 let swiftPath = CommandLine.arguments[2]
 let swiftUtl = URL(fileURLWithPath: swiftPath)
+
+print("Generating: \(fbsPath) -> \(swiftPath)")
 
 let schema = fileContent.withUnsafeBytes { (p: UnsafePointer<UInt8>) -> Schema? in
     return Schema.with(pointer: p, length: fileContent.count)?.0
@@ -43,5 +41,8 @@ let schema = fileContent.withUnsafeBytes { (p: UnsafePointer<UInt8>) -> Schema? 
 
 if let swiftFileContent = schema?.swift {
     try!swiftFileContent.data(using: .utf8)?.write(to: swiftUtl)
+    print("✅ Completed")
+} else {
+    print("❌ Could not generate")
 }
 
